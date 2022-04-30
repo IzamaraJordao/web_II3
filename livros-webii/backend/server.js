52844e577f4bc2043f18f06a02f6e1626268229f
IzamaraJordao <89167184+IzamaraJordao@users.noreply.github.com>
2022-04-29 22:01:48 -0300

    node

--- livros-webii/backend/server.js
+++ livros-webii/backend/server.js
@@ -1,10 +1,10 @@
 const http = require('http') // instanciou o protocolo http
-const servidor = require('./config/aplicativo.js') // aqui vai o arquivo que cria o server
+const servidor = require('./src/config/aplicativo.js') // aqui vai o arquivo que cria o server
 
 
-require('./src/databases/indexDb')
+require('./src/database/indexDb')
 
-http.createServer(servidor).listen(servidor.get('porta'), () => { //aqui ele só vai ouvir a porta
+http.createServer(servidor).listen(servidor.get('porta'), function() { //aqui ele só vai ouvir a porta
     console.log(`Servidor rodando na porta ${server.get('url')}${server.get('porta')}`) //aqui ele vai mostrar na tela
 })
 
